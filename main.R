26c5599f43ed798baeb25839b76df98a4ee5308c
Dyer <James.Dyer@edfenergy.com>
2024-10-07 10:30:01 +0100

    added some initial exploratory analysis lines

--- main.R
+++ main.R
@@ -123,8 +123,31 @@ write_csv(df_combined, file.path(base_path, paste0("exported-data/df_combined_",
 
 
 
+df_mop |> count(msn,name="n_msn") |> 
+  count(n_msn)
+
+df_mop |> group_by(msn) |> filter(n()==4)
+
+.Last.value |> arrange(msn)
+
+.Last.value[1:2,] |> glimpse()
+
+df_mop |> 
+  group_by(msn) |> 
+  summarise(n=n(),n_mpan=n_distinct(mpan), n_start=n_distinct(mop_start)) |> 
+  count(n,n_mpan,n_start) |>
+  arrange(n,n_mpan,n_start) |> 
+  print(n=100)
 
 
+df_mop |> 
+  group_by(msn) |> 
+  summarise(n=n(),n_mpan=n_distinct(mpan), n_start=n_distinct(mop_start)) |> 
+  group_by(n,n_mpan,n_start) |> 
+  sample_n(1) |> 
+  ungroup() |> 
+  left_join(df_mop,by="msn") |> 
+  arrange(n,n_mpan,n_start,msn,mpan)
 
 
 
